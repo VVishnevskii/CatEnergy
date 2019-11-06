065c0a0f852cdb4e54748f88e43a20eaf3ca7e25
VVishnevskii <50050949+VVishnevskii@users.noreply.github.com>
2019-11-06 18:46:07 +0300

    Разметка футера, стилизация планшетной версии

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-var buttonPageHeader = document.querySelector('.top-header__button');
-var mainNavMenu = document.querySelector('.main-nav--mobile');
 
-buttonPageHeader.addEventListener('click', function() {
-  var mainNavStatus = document.querySelector('.hidden');
-  if (mainNavStatus) {
-    mainNavMenu.classList.remove('hidden');
-    buttonPageHeader.classList.add('active');
-  }
-  else {
-    mainNavMenu.classList.add('hidden');
-    buttonPageHeader.classList.remove('active');
-  }
-})
+  let buttonPageHeader = document.querySelector('.top-header__button');
+  let mainNavMenu = document.querySelector('.main-nav--mobile');
+
+  buttonPageHeader.addEventListener('click', function() {
+    let mainNavStatus = document.querySelector('.hidden');
+    if (mainNavStatus) {
+      mainNavMenu.classList.remove('hidden');
+      buttonPageHeader.classList.add('active');
+    }
+    else {
+      mainNavMenu.classList.add('hidden');
+      buttonPageHeader.classList.remove('active');
+    }
+  })
 
 
 
